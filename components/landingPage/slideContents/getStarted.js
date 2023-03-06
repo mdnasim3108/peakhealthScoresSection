c1285385796f7d8c6d602de70004582417614280
mdnasim3108 <91870926+mdnasim3108@users.noreply.github.com>
2023-03-06 17:05:40 +0530

    Update getStarted.js

--- components/landingPage/slideContents/getStarted.js
+++ components/landingPage/slideContents/getStarted.js
@@ -1,35 +1,38 @@
 import getStartImage from "../../../public/getStart.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import Image from "next/image"
+import Image from "next/image";
 import { useState } from "react";
 const GetStarted = (props) => {
   const [showArrow, setShowArrow] = useState(false);
   return (
     <div className="flex flex-col justify-center items-center">
-      <Image src={getStartImage} className="xl:w-[20rem] md:w-[16rem] mb-4" />
+      <h1 className="font-bold mb-5 text-4xl">What’s my Work Stress level?</h1>
+      <h2 className="text-xl text-gray-500 relative bottom-5">
+        Find out with Science and AI powered Stress Assistant!{" "}
+      </h2>
+      <Image src={getStartImage} className="xl:w-[20rem] md:w-[16rem] mb-4" alt="getStart" />
       <h1 className="text-blue text-lg mb-2">STEP 1</h1>
-      <h1 className="font-bold mb-5 text-3xl">
-        Check Your Job Stress Level For Today
-      </h1>
       <p className="font-sans text-lg mb-5 text-center">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book.
+        Our
+        AI-powered Stress Assistant measures your stress level and generates
+        personalized recommendations for you
+      </p>
+      <p className="font-sans text-lg mb-5 text-center">
+      Don't let job stress control you - take control today!
       </p>
       <button
-        className=" text-blue-500 border border-blue-500 hover:bg-blue-500 hover:text-white font-bold uppercase px-8 py-3 rounded outline-none focus:outline-none mr-1 mb-[0.8rem] md:mb-1 ease-linear transition-all duration-150"
+        className="border text-lg border-violet-500 bg-violet-500 hover:bg-violet-600 text-white font-bold px-8 py-3 rounded outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
         onMouseEnter={() => setShowArrow((prev) => !prev)}
         onMouseLeave={() => setShowArrow((prev) => !prev)}
         onClick={props.move}
       >
-        Check Now
-        {showArrow && 
+        Start my 1 min Stress Check
+        {showArrow && (
           <i className="ml-[0.5rem] arrowIcon">
             <FontAwesomeIcon icon={faArrowRight} />
           </i>
-        }
+        )}
       </button>
     </div>
   );
