148710f5f7b94c79204fa3f235db27c0ebdc1cf3
mdnasim3108 <91870926+mdnasim3108@users.noreply.github.com>
2023-03-06 17:09:07 +0530

    Update stressScore.js

--- components/landingPage/slideContents/stressScore.js
+++ components/landingPage/slideContents/stressScore.js
@@ -1,31 +1,29 @@
-// import ReactSpeedometer from "react-d3-speedometer";
-import { useState, useContext } from "react";
+import { useState, useContext,useEffect } from "react";
 import dynamic from "next/dynamic";
 import voiceContext from "../contextStrore/voiceContext";
 const ReactSpeedometer = dynamic(() => import("react-d3-speedometer"), {
   ssr: false,
 });
 const StressScore = (props) => {
+  const texts=["Stress Assistant AI is analyzing your voice…","Computing Your Stress Level…","Generating Your Stress Report…"]
+  const [status,setStatus]=useState(0)
   const voiceState = useContext(voiceContext);
-  const overallScore = !voiceState.voiceFeatures.loading
+  useEffect(()=>{
+      setInterval(()=>{
+        setStatus((prev)=>{
+          if(prev===2) return 0       
+          return prev+1
+        })
+      },10000)
+  },[])
+  const overallScore = voiceState.voiceFeatures.loading
     ? voiceState.voiceFeatures.score
-    : 50;
+    : 40;
   const labels = [
-    {
-      text: "Very Bad",
-      position: "INSIDE",
-      color: "#555",
-    },
     {
       text: "Bad",
       position: "INSIDE",
       color: "#555",
-    },
-    {
-      text: "Ok",
-      position: "INSIDE",
-      color: "#555",
-      fontSize: "19px",
     },
     {
       text: "Good",
@@ -33,65 +31,85 @@ const StressScore = (props) => {
       color: "#555",
     },
     {
-      text: "Very Good",
+      text: "Great",
+      position: "INSIDE",
+      color: "#555",
+    },
+    {
+      text: "Awesome",
       position: "INSIDE",
       color: "#555",
     },
   ];
   const results = (
-    <div className=" px-0 md:px-10 recording md:relative top-10 text-center">
-      <h1 className="xl:text-4xl lg:text-2xl font-bold font-serif">
+    <div className=" px-0 md:px-10 recording md:relative text-center">
+      <h1 className="xl:text-4xl lg:text-2xl font-bold">
         Your Work Stress Score for today
       </h1>
-      <div className="bg-gray-400 w-full h-8 rounded-full mt-10">
+      <div className="bg-gradient-to-r mt-20 rounded flex justify-end from-green-600 via-orange-500 to-red-500 scoreFill w-full h-8 transition-all duration-500 ease-linear mt-10">
         <div
-          className="h-full transition-all duration-500 ease-linear rounded-full bg-gradient-to-r from-red-600 to-green-500 scoreFill"
-          style={{ width: `${overallScore}%` }}
+          className="h-full bg-gray-200  overlay rounded-r"
+          style={{ width: `${100-overallScore}%` }}
         ></div>
       </div>
+      <div className="overlay"/>
       <div className="flex justify-between">
-        <p className="text-sm text-gray-400">High Stress</p>
-        <p className="text-sm text-gray-400">Medium Stress</p>
         <p className="text-sm text-gray-400">Low Stress</p>
+        <p className="text-sm text-gray-400">Medium Stress</p>
+        <p className="text-sm text-gray-400">High Stress</p>
       </div>
-      <div className="flex justify-center items-center md:justify-around md:flex-row flex-col">
+      <div className="flex justify-center items-center md:justify-around md:flex-row flex-col mt-[3rem]">
         <ReactSpeedometer
           value={
-            !voiceState.voiceFeatures.loading &&
-            +voiceState.voiceFeatures.energy * 10
+            !voiceState.voiceFeatures.loading?
+              voiceState.voiceFeatures.energy:0
           }
+          minValue={0}
+          maxValue={36}
+          segments={4}
           currentValueText="Energy"
           customSegmentLabels={labels}
-          labelFontSize="10px"
+          segmentColors={["#ff927e",
+          "#17ffec",
+          "#bee74b",
+          "#7bda40",]}
+          width={350}
+          ringWidth={40}
         />
         <ReactSpeedometer
           value={
-            !voiceState.voiceFeatures.loading &&
-            +voiceState.voiceFeatures.control * 10
+            !voiceState.voiceFeatures.loading?
+            Number(voiceState.voiceFeatures.live):0
           }
-          currentValueText="Control"
+          minValue={0}
+          maxValue={0.45}
+          segments={4}
+          currentValueText="Liveliness"
           customSegmentLabels={labels}
-          labelFontSize="10px"
+          segmentColors={["#ff927e",
+          "#17ffec",
+          "#bee74b",
+          "#7bda40",]}
+          width={350}
+          ringWidth={40}
         />
       </div>
       <div className="relative bottom-20">
-        <h1 className="text-xl font-sans font-bold mb-10">
-          Unmanaged Stress can lead to Burnout
-        </h1>
         <button
-          className="rounded bg-green-500 p-2 text-white text-lg font-sans"
+          className="rounded border-violet-500 bg-violet-500 hover:bg-violet-600 mt-3 p-2 text-white text-lg font-sans"
           onClick={props.move}
         >
-          Check my Burnout Level
+          Check my personalized recommendations  
         </button>
       </div>
     </div>
   );
   const loadContent = (
-    <div className="bg-gray-200 opacity-50 flex justify-center items-center h-full">
-      <h1 className="text-lg font-sans font-bold status">
-        {voiceState.voiceFeatures.loading && voiceState.voiceFeatures.status}
+    <div className="bg-gray-200 opacity-50  h-full lg:pt-10 text-center">
+      <h1 className="text-3xl font-sans font-bold status">
+        {voiceState.voiceFeatures.loading && texts[status]}
       </h1>
+      <h1 className="text-2xl font-sans font-bold mt-[7rem]">Guess What's your stress level!!!</h1>
     </div>
   );
 
