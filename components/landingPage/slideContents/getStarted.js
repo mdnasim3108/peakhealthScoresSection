13d8bda84eaf22680dac17d1bad1dc40da1d89df
mdnasim3108 <91870926+mdnasim3108@users.noreply.github.com>
2023-03-06 17:25:23 +0530

    Update getStarted.js

--- components/landingPage/slideContents/getStarted.js
+++ components/landingPage/slideContents/getStarted.js
@@ -7,22 +7,22 @@ const GetStarted = (props) => {
   const [showArrow, setShowArrow] = useState(false);
   return (
     <div className="flex flex-col justify-center items-center">
-      <h1 className="font-bold text-4xl">What’s my Work Stress level?</h1>
+      <h1 className="font-bold mb-5 text-4xl">What’s my Work Stress level?</h1>
       <h2 className="text-xl text-gray-500 relative bottom-5">
         Find out with Science and AI powered Stress Assistant!{" "}
       </h2>
-      <Image src={getStartImage} className="xl:w-[16rem] md:w-[16rem]" alt="getStart" />
+      <Image src={getStartImage} className="xl:w-[20rem] md:w-[16rem] mb-4" alt="getStart" />
       <h1 className="text-blue text-lg mb-2">STEP 1</h1>
       <p className="font-sans text-lg mb-5 text-center">
         Our
         AI-powered Stress Assistant measures your stress level and generates
         personalized recommendations for you
       </p>
-      <p className="font-sans text-lg text-center">
+      <p className="font-sans text-lg mb-5 text-center">
       Don't let job stress control you - take control today!
       </p>
       <button
-        className="border text-lg border-violet-500 bg-violet-500 relative top-5 hover:bg-violet-600 text-white font-bold px-8 py-3 rounded outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+        className="border text-lg border-violet-500 bg-violet-500 hover:bg-violet-600 text-white font-bold px-8 py-3 rounded outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
         onMouseEnter={() => setShowArrow((prev) => !prev)}
         onMouseLeave={() => setShowArrow((prev) => !prev)}
         onClick={props.move}
