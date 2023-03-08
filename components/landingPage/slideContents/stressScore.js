2511ecb33f211a7e353a89c2559ddfe7dda12626
mdnasim3108 <91870926+mdnasim3108@users.noreply.github.com>
2023-03-08 19:56:50 +0530

    Update stressScore.js

--- components/landingPage/slideContents/stressScore.js
+++ components/landingPage/slideContents/stressScore.js
@@ -1,21 +1,15 @@
-import { useState, useContext,useEffect } from "react";
+import { useState, useContext, useEffect } from "react";
 import dynamic from "next/dynamic";
 import voiceContext from "../contextStrore/voiceContext";
+import Slider from "@mui/material/Slider";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import DynamicText from "./dynamicText";
 const ReactSpeedometer = dynamic(() => import("react-d3-speedometer"), {
   ssr: false,
 });
 const StressScore = (props) => {
-  const texts=["Stress Assistant AI is analyzing your voice…","Computing Your Stress Level…","Generating Your Stress Report.."]
-  const [status,setStatus]=useState(0)
   const voiceState = useContext(voiceContext);
-  useEffect(()=>{
-      setInterval(()=>{
-        setStatus((prev)=>{
-          if(prev===2) return 0       
-          return prev+1
-        })
-      },10000)
-  },[])
+
   const overallScore = voiceState.voiceFeatures.loading
     ? voiceState.voiceFeatures.score
     : 40;
@@ -41,6 +35,26 @@ const StressScore = (props) => {
       color: "#555",
     },
   ];
+  const useStyles = makeStyles({
+    root: {
+      width: 200,
+    },
+  });
+
+  const CustomSlider = withStyles({
+    // rail: {
+    //   backgroundColor: "gray"
+
+    // },
+    // track: {
+    //   backgroundImage: "linear-gradient(to right,#76ff03,orange,red)"
+
+    // },
+    thumb: {
+      height: 30,
+      width: 30,
+    },
+  })(Slider);
   const results = (
     <div className=" px-0 md:px-10 recording md:relative text-center">
       <h1 className="xl:text-4xl lg:text-2xl font-bold">
@@ -49,10 +63,10 @@ const StressScore = (props) => {
       <div className="bg-gradient-to-r mt-20 rounded flex justify-end from-green-600 via-orange-500 to-red-500 scoreFill w-full h-8 transition-all duration-500 ease-linear mt-10">
         <div
           className="h-full bg-gray-200  overlay rounded-r"
-          style={{ width: `${100-overallScore}%` }}
+          style={{ width: `${100 - overallScore}%` }}
         ></div>
       </div>
-      <div className="overlay"/>
+      <div className="overlay" />
       <div className="flex justify-between">
         <p className="text-sm text-gray-400">Low Stress</p>
         <p className="text-sm text-gray-400">Medium Stress</p>
@@ -61,35 +75,31 @@ const StressScore = (props) => {
       <div className="flex justify-center items-center md:justify-around md:flex-row flex-col mt-[3rem]">
         <ReactSpeedometer
           value={
-            !voiceState.voiceFeatures.loading?
-              voiceState.voiceFeatures.energy:0
+            !voiceState.voiceFeatures.loading
+              ? voiceState.voiceFeatures.energy
+              : 0
           }
           minValue={0}
           maxValue={36}
           segments={4}
           currentValueText="Energy"
           customSegmentLabels={labels}
-          segmentColors={["#ff927e",
-          "#17ffec",
-          "#bee74b",
-          "#7bda40",]}
+          segmentColors={["#ff927e", "#17ffec", "#bee74b", "#7bda40"]}
           width={350}
           ringWidth={40}
         />
         <ReactSpeedometer
           value={
-            !voiceState.voiceFeatures.loading?
-            Number(voiceState.voiceFeatures.live):0
+            !voiceState.voiceFeatures.loading
+              ? Number(voiceState.voiceFeatures.live)
+              : 0
           }
           minValue={0}
           maxValue={0.45}
           segments={4}
           currentValueText="Liveliness"
           customSegmentLabels={labels}
-          segmentColors={["#ff927e",
-          "#17ffec",
-          "#bee74b",
-          "#7bda40",]}
+          segmentColors={["#ff927e", "#17ffec", "#bee74b", "#7bda40"]}
           width={350}
           ringWidth={40}
         />
@@ -99,17 +109,38 @@ const StressScore = (props) => {
           className="rounded border-violet-500 bg-violet-500 hover:bg-violet-600 mt-3 p-2 text-white text-lg font-sans"
           onClick={props.move}
         >
-          Check my personalized recommendations  
+          Check my personalized recommendations
         </button>
       </div>
     </div>
   );
   const loadContent = (
     <div className="bg-gray-200 opacity-50  h-full lg:pt-10 text-center">
-      <h1 className="text-3xl font-sans font-bold status">
-        {voiceState.voiceFeatures.loading && texts[status]}
+      <h1 className="text-xl">Your Stress Assistant AI is...</h1>
+      <DynamicText
+        texts={[
+          " analyzing your voice",
+          "computing your stress level",
+          "generating your stress report",
+        ]}
+      />
+      <h1 className="text-2xl font-sans font-bold mt-[7rem] mb-5">
+        Can you guess, what's your stress level!!!
       </h1>
-      <h1 className="text-2xl font-sans font-bold mt-[7rem]">Guess What's your stress level!!!</h1>
+      <p className="relative bottom-5">(Slide the marker)</p>
+      <div className="w-[70%] mx-auto">
+        <CustomSlider
+          className="horizontal-slider h-[1.3rem]"
+          mountOnEnter
+          unmountOnExit
+          onChange={(e) => voiceState.guessScore(e.target.value)}
+        />
+        <div className="flex justify-between relative bottom-3">
+          <p>Low Stress</p>
+          <p>Medium Stress</p>
+          <p>high Stress</p>
+        </div>
+      </div>
     </div>
   );
 
