cabfda2e45805c374988ef66aa57fe360bcec525
Peakhealth <support@peakhealth.tech>
2023-03-16 01:09:05 +0530

    googleAnalytics2

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,27 @@
 import '@/styles/globals.css'
 import Script from 'next/script'
 
-
+// import { GoogleAnalytics } from 'nextjs-google-analytics'
 const App = ({ Component, pageProps }) => {
 
 
   return (
     <>
-    <Script async  src="https://www.googletagmanager.com/gtag/js?id=G-64VSBXJZV4" ></Script>
-    
-    <Script>
-      { ` 
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-64VSBXJZV4');
-        
-      `}
-    </Script>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-64VSBXJZV4"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
 
+          gtag('config', 'G-64VSBXJZV4');
+        `}
+      </Script>
+
+{/* <GoogleAnalytics gaMeasurementId='G-64VSBXJZV4' /> */}
 
       <Component {...pageProps} />
     </>
