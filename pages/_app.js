a419e5cbcffa9d2339a8f0fc9d7b2afe190e5a14
Peakhealth <support@peakhealth.tech>
2023-04-26 20:29:52 +0530

    clarity

--- pages/_app.js
+++ pages/_app.js
@@ -25,6 +25,18 @@ const App = ({ Component, pageProps }) => {
   }
     </Script>
    
+
+    <Script type="text/javascript">
+       {` 
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "gu2a7l52va");
+      `}
+    </Script>
+
+
     <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-64VSBXJZV4"
         strategy="afterInteractive"
