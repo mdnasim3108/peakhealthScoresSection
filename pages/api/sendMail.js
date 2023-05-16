9132230db132cd5839874c98d8c477b692a1c35c
peakHealth <support@peakhealth.tech>
2023-05-16 19:28:44 +0530

    send

--- pages/api/sendMail.js
+++ pages/api/sendMail.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
         await sendgrid.send(emailData);
         res.status(200).json({ message: 'Email sent successfully' });
       } catch (error) {
-        res.status(500).json({ message: 'Failed to send email' });
+        res.json({ message:error });
         console.log(error)
       }
     } else {
